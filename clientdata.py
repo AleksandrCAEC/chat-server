8b7bad3b8afc81f942dce14f795abaf738bba3fe
AleksandrCAEC <caecgmbh@gmail.com>
2025-02-05 17:13:03 +0100

    Update clientdata.py

--- clientdata.py
+++ clientdata.py
@@ -11,7 +11,7 @@ logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[
-        logging.FileHandler("app.log"),
+        logging.FileHandler("clientdata.log"),
         logging.StreamHandler()
     ]
 )
